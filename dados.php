65b077c48cee5fe4c859289867c9b37629159f81
ISC0901 <166071589+ISC0901@users.noreply.github.com>
2025-05-28 16:06:33 -0300

    Add files via upload

---
+++ dados.php
@@ -0,0 +1,11 @@
+<?php
+$d1=$_POST['usuario'] ;
+$d2=$_POST['email'];
+$d3=$_POST['senha'];
+$d4=$_POST['csenha']
+
+echo " Dado ".$d1."<br>";
+echo "Dado ".$d2."<br>";
+echo " Dado ".$d3."<br>";
+echo " Dado".$d4."<br>";
+?>
